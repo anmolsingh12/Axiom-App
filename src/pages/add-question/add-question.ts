567be49f819be5a42e01f44a45802b28490618f4
Rajeshgupta123 <37134558+Rajeshgupta123@users.noreply.github.com>
2018-06-29 18:11:06 +0530

    New Commit

--- src/pages/add-question/add-question.ts
+++ src/pages/add-question/add-question.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams} from 'ionic-angular';
 
 
@@ -9,14 +9,36 @@ import { IonicPage, NavController, NavParams} from 'ionic-angular';
 })
 export class AddQuestionPage {
 
+  // @ViewChild("qType")qType;
+  // @ViewChild("qSubType")qSubType1;
+
   board:Array<{name:string, alias:string }>;
   class:Array<{name:string, value:string}>;
   subject:Array<{name:string}>;
   topic:Array<{name:string}>;
   sub_topic:Array<{name:string}>;
   qSubType:Array<{name:any}>;
-  subjective:boolean=false;
-  objective:boolean=false;
+  subjective:boolean = false;
+  objective:boolean = false;
+  checkFill:boolean = false;
+  checkIdentify:boolean = false;
+  checkOneLine:boolean = false;
+  checkVSA:boolean = false;
+  checkSA:boolean = false;
+  checkLA:boolean = false;
+  checkVLA:boolean = false;
+  checkMcqSingle:boolean = false;
+  checkMcqMultiple:boolean = false;
+  checkTF:boolean = false;
+  checkMatch:boolean = false;
+  checkMatrix:boolean = false;
+
+  form:Array<{brd:string, class:string, subject:string, topic:string,
+               subtopic:string, qType:string, qSubType:string, skillType:string,
+               difficultyLevel:string, marks:number, negativeMarks:number, time:number,
+               reference:string}>;
+
+  questionDetails:Array<{question:string, answer:string, explaination:string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) 
   {
@@ -47,33 +69,244 @@ export class AddQuestionPage {
                   
   }
 
+  logForm()
+  {
+    console.log(this.form+"   "+this.questionDetails );
+  }
+
   check(value:any)
   {  
     console.log(value);
     if(value =="Subjective")
     {
       this.qSubType = [{name:'Fill in The Blanks'},
+                      {name:'Identify'},
                       {name:'One Line'},
                       {name:'Very Short Answer'},
                       {name:'Short Answer'},
                       {name:'Long Answer'},
                       {name:'Very Long Answer'}];
-      
-    }
-
-    
+    }   
     if(value =="Objective")
     {
       this.qSubType = [{name:'MCQ Single Response'},
                       {name:'MCQ Multiple Response'},
-                      {name:'True False'},
-                      {name:'Matrix Match'}];
-      
-    }
-
-    
+                      {name:'True/False'},
+                      {name:'Match The Followings'},
+                      {name:'Matrix Match'}]; 
+    }    
   }
 
+  checkOption(val:any)
+  {
+    if(val == "Fill in The Blanks")
+    {
+      this.checkFill = true;
+      this.checkIdentify = false;
+      this.checkOneLine = false;
+      this.checkVSA = false;
+      this.checkSA = false;
+      this.checkLA = false;
+      this.checkVLA = false;
+      this.checkMcqSingle = false;
+      this.checkMcqMultiple = false;
+      this.checkTF = false;
+      this.checkMatch = false;
+      this.checkMatrix = false;
+    }
+
+    else if(val == "Identify")
+    {
+      this.checkIdentify = true;
+      this.checkFill = false;
+      this.checkMatch = false;
+      this.checkOneLine = false;
+      this.checkVSA = false;      
+      this.checkSA = false;
+      this.checkMatrix = false;
+      this.checkLA = false;
+      this.checkVLA = false;
+      this.checkMcqSingle = false;
+      this.checkMcqMultiple = false;
+      this.checkTF = false;
+    }
+    
+    else if(val == "One Line")
+    {
+      this.checkOneLine = true;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkVSA = false;
+      this.checkSA = false;
+      this.checkMatrix = false;
+      this.checkMatch = false;
+      this.checkLA = false;
+      this.checkVLA = false;
+      this.checkTF = false;
+      this.checkMcqSingle = false;
+      this.checkMcqMultiple = false;
+    }
+
+    else if(val == "Very Short Answer")
+    {
+      this.checkVSA = true;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+      this.checkLA = false;
+      this.checkTF = false;
+      this.checkMatrix = false;
+      this.checkMatch = false;
+      this.checkVLA = false;
+      this.checkMcqSingle = false;
+      this.checkMcqMultiple = false;
+    }
+
+    else if(val == "Short Answer")
+    {
+      this.checkVSA = true;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+      this.checkTF = false;
+      this.checkLA = false;
+      this.checkVLA = false;
+      this.checkMatrix = false;
+      this.checkMatch = false;
+      this.checkMcqSingle = false;
+      this.checkMcqMultiple = false;
+    }
+
+    else if(val == "Very Short Answer")
+    {
+      this.checkVSA = true;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+      this.checkLA = false;
+      this.checkTF = false;
+      this.checkVLA = false;
+      this.checkMatch = false;
+      this.checkMatrix = false;
+      this.checkMcqSingle = false;
+      this.checkMcqMultiple = false;
+    }
+
+    else if(val == "Long Answer")
+    {
+      this.checkLA = true;
+      this.checkVSA = false;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+      this.checkTF = false;
+      this.checkVLA = false;
+      this.checkMatrix = false;
+      this.checkMatch = false;
+      this.checkMcqSingle = false;
+      this.checkMcqMultiple = false;   
+    }
+
+    else if(val == "Very Long Answer")
+    {
+      this.checkVLA = true;
+      this.checkLA = false;
+      this.checkVSA = false;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+      this.checkTF = false;
+      this.checkMatrix = false;
+      this.checkMatch = false;
+      this.checkMcqSingle = false;
+      this.checkMcqMultiple = false;
+    }
+
+    else if(val == "MCQ Single Response")
+    {
+      this.checkMcqSingle = true;
+      this.checkVLA = false;
+      this.checkLA = false;
+      this.checkVSA = false;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkMatrix = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+      this.checkMatch = false;
+      this.checkTF = false;
+      this.checkMcqMultiple = false;
+    }
+
+    else if(val == "MCQ Multiple Response")
+    {
+      this.checkMcqMultiple = true;
+      this.checkMcqSingle = false;
+      this.checkVLA = false;
+      this.checkLA = false;
+      this.checkVSA = false;
+      this.checkMatrix = false;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+      this.checkMatch = false;
+      this.checkTF = false;
+    }
+    else if(val == "True/False")
+    {
+      this.checkTF = true;
+      this.checkMcqMultiple = false;
+      this.checkMcqSingle = false;
+      this.checkVLA = false;
+      this.checkLA = false;
+      this.checkVSA = false;
+      this.checkMatrix = false;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkMatch = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+    }
+    else if (val == "Match The Followings")
+    {
+      this.checkMatch = true;
+      this.checkTF = false;
+      this.checkMcqMultiple = false;
+      this.checkMcqSingle = false;
+      this.checkVLA = false;
+      this.checkMatrix = false;
+      this.checkLA = false;
+      this.checkVSA = false;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+    }
+
+    else if(val=="Matrix Match")
+    {
+      this.checkMatrix = true;
+      this.checkMatch = false;
+      this.checkTF = false;
+      this.checkMcqMultiple = false;
+      this.checkMcqSingle = false;
+      this.checkVLA = false;
+      this.checkLA = false;
+      this.checkVSA = false;
+      this.checkOneLine = false;
+      this.checkFill = false;
+      this.checkIdentify = false;
+      this.checkSA = false;
+    }
+  }
+
+  
   
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddQuestionPage');
